67a2aae1e9844545a308ab2d8d2fd894f5ec573b
brixxt27 <brixxt27@gmail.com>
2026-02-05 17:52:45 +0900

    제곱수 구하기 구현 완료

--- baaarkingDog/1-4-2.cpp
+++ baaarkingDog/1-4-2.cpp
@@ -1,3 +1,22 @@
 #include <iostream>
 
-int main() {}
+using namespace std;
+
+/**
+ * int N을 인자로 받아 N을 포함한 작은 수 중 가장 큰 제곱수를 반환하는 함수
+ */
+int func(int N) {
+  // N을 넘지 않을 때까지 2의 제곱을 반복합니다.
+  int num = 1;
+  while (num * 2 <= N) {
+    num *= 2;
+  }
+  return num;
+}
+
+int main() {
+  // 10억 이하의 N
+  int N;
+  cin >> N;
+  cout << func(N) << endl;
+}
