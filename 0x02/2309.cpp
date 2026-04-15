220a1e7f080a177f5d8e6d435b8b3895b7c75247
brixxt27 <brixxt27@gmail.com>
2026-04-15 23:48:59 +0900

    브루트포스에서 변형을 하여 더 적은 경우를 찾아 구하기
    - 문제에서 합이 100이 아닌 경우는 절대 나오지 않으므로 반대로 수를 전부 더한 뒤 두 개만 지워서 100이 되는 수를 찾습니다.
    - 이는 뽑는 순서가 상관 없이 n[1], n[2]와 n[2], n[1]이 동일한 수열입니다. 때문에 두 번째 for문에서 단순히 b를 1부터 시작하면 안 되고, a + 1로 둬야 같은 경우를 다루지 않습니다.
    - 그리고 100인 수를 찾으면 거기에서 바로 반환하여 프로그램을 종료시켜줘야 다른 경우를 이어서 찾지 않습니다.

--- 0x02/2309.cpp
+++ 0x02/2309.cpp
@@ -1,20 +1,39 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 using namespace std;
 
 int main() {
 	// 입력을 숫자 9개 받기
-	vector<int> v;
+	// 9개 중에서 7개를 더했을 때 100인 경우 찾기
+	// 합이 100인 7개의 수를 오름차순으로 정렬해서 출력하기, 
+		// 100이 되는 조합이 있다면 그중 아무 거나 출력, 100이 되지 않는 경우는 없다.
+	// 9개의 수를 모두 더한 다음, 
+		// 두 개의 수를 뺐을 때 100인 수를 찾기
+	int n[9];
+	int sum = 0;
 	for (int i = 0; i < 9; i++) {
-		int n;
-		cin >> n;
-		v.push_back(n);
+		cin >> n[i];
+		sum += n[i];
 	}
-	for (int i = 0; i < 9; i++) {
-		for (int j = 1; j < 9; j++) {
+	sort(n, n + 9);
+
+	// sum에 9개의 수 모두 더한다
+	// 두 개의 수를 조합하여 sum에서 뺀다.
+	// 100이라면 sum에서 뺀 두 개를 제외한 나머지 수를 출력한다.
+	// 100이 아니라면 기존 sum에서 다른 수를 조합하여 다시 뺀다. 이를 반복한다.
+	int result;
+	for (int a = 0; a < 8; a++) {
+		for (int b = a + 1; b < 9; b++) {
+			result = sum;
+
+			result -= (n[a] + n[b]);
+			if (result == 100) {
+				for (int c = 0; c < 9; c++) {
+					if (c != a && c!= b)
+						cout << n[c] << "\n";
+				}
+				return 0;
+			}
 		}
 	}
-	// 더해보면서 합이 100인 수를 찾기, 100이 안 되는 경우는 없다
-	// 7개 숫자의 합이 100인 수를 찾으면 이를 오름차 순으로 정렬하여 출력하기
 }
