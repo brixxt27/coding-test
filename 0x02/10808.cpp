1c85504244ef87b5290ddf89d5e128ea48fc3f0e
brixxt27 <brixxt27@gmail.com>
2026-04-17 18:29:03 +0900

    연습 문제1
    - 만약 arr를 지역변수로 선언 했다면 int arr[] = {}이나 fill(arr, arr + 26, 0)이 필요했을 것이다. 전역변수이기 때문에 자동으로 0으로 초기화 된다.

---
+++ 0x02/10808.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int arr[26];
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	string s;
+
+	cin >> s;
+	for (char c : s) {
+		arr[c - 'a']++;
+	}
+	for (int i = 0; i < 26; i++) {
+		cout << arr[i] << " ";
+	}
+}
